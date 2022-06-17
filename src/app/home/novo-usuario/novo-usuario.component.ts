72926a1203820795230ebc90522a70a206be0081
jviitsm <jvictorgmodesto@gmail.com>
2022-06-16 22:54:45 -0300

    validacoes padroes

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,6 +1,6 @@
 import { Usuario } from './usuario';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
@@ -17,8 +17,16 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
+      email: ['',
+      [
+        Validators.required,
+        Validators.email
+      ]],
+      fullName: ['',
+      [
+        Validators.required,
+         Validators.minLength(4)
+      ]],
       userName: [''],
       password: ['']
     });
