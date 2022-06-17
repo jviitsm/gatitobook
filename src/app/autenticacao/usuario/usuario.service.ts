6f2ff77d94b7b0ef64146231685d05dbb8e8bec9
jviitsm <jvictorgmodesto@gmail.com>
2022-06-17 13:37:43 -0300

    servico de usuario implementado

--- src/app/autenticacao/usuario/usuario.service.ts
+++ src/app/autenticacao/usuario/usuario.service.ts
@@ -1,9 +1,44 @@
+import { IUsuario } from './usuario';
+import { TokenService } from './../token.service';
 import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  private usuarioSubject = new BehaviorSubject({});
+
+  constructor(private tokenService: TokenService) {
+    if(this.tokenService.possuiToken()){
+      this.decodificaJWT();
+    }
+   }
+
+  private decodificaJWT(): void{
+    const token = this.tokenService.retornaToken();
+    const usuario = jwtDecode(token) as IUsuario;
+    return this.usuarioSubject.next(usuario)
+  }
+
+  retornaUsuario(): Observable<IUsuario>{
+    return this.usuarioSubject.asObservable();
+  }
+
+  salvaToken(token: string): void{
+    this.tokenService.salvaToken(token);
+    return this.decodificaJWT();
+  }
+
+  logout(): void{
+    this.tokenService.excluiToken();
+    return this.usuarioSubject.next({});
+  }
+
+  estaLogado(): boolean{
+    return this.tokenService.possuiToken();
+  }
+
 }
