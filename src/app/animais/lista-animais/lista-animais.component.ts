5ede83fa62573ef391f86f9b8c3ec6073e4fcab2
joao modesto <jvictorgmodesto@gmail.com>
2022-06-17 17:29:26 -0300

    pipe async

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { Animais } from '../animais';
 import { AnimaisService } from '../animais.service';
@@ -10,22 +12,20 @@ import { AnimaisService } from '../animais.service';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  animais !: Animais;
+  animais$ !: Observable<Animais>;
 
   constructor(private usuarioService: UsuarioService, private animaisService: AnimaisService) { }
 
   ngOnInit(): void {
-    this.usuarioService.retornaUsuario().subscribe(
-      (usuario) => {
-        const userName = usuario.name ?? '';
-        this.animaisService.listaDoUsuario(userName).subscribe(
-          (animais) => {
-            this.animais = animais;
-          }
-        );
 
-      }
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
+          const userName = usuario.name ?? '';
+          return this.animaisService.listaDoUsuario(userName);
+        }
+      )
     )
+
   }
 
 
