bd26bee489cc8738dece3860ac6f41072a39026b
jviitsm <jvictorgmodesto@gmail.com>
2022-06-17 13:49:12 -0300

    salvando token no autenticarservice

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,18 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
+import {tap} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacaoService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private usuarioService: UsuarioService) { }
 
   autenticar(usuario: string, senha: string): Observable<any> {
     return this.httpClient.post('http://localhost:3000/login', {
       usuario,
-      senha
-    });
-  }
+      senha}
+      ,{observe: 'response'}).
+      pipe(
+        tap(
+          (res) => {
+            const authToken = res.headers.get('x-access-token') ?? '';
+            this.usuarioService.salvaToken(authToken);
+          }
+        ));
+      }
 }
