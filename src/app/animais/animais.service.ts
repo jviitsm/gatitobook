1e4d634e617c857ab246918a9f08f9e5e60cdded
joao modesto <jvictorgmodesto@gmail.com>
2022-06-18 18:27:08 -0300

    salvar animal implementado

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -34,7 +34,19 @@ export class AnimaisService {
     ,{observe: 'response' }).pipe(mapTo(true), catchError((error) => {
       return error.status === NOT_MODIFIED ? of(false) : throwError(error);
     }))
-
   }
+
+   upload(descricao: string, permiteComentario: boolean, arquivo: File){
+    const formData = new FormData();
+    formData.append('description', descricao);
+    formData.append('allowComments', permiteComentario ? 'true' : 'false');
+    formData.append('imageFile', arquivo);
+
+    return this.httpClient.post(`${API}/photos/upload`, formData,
+    { observe: 'events',
+      reportProgress: true,
+    });
+
+   }
 
 }
